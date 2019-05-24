16b11abb058e01f14d13a5de3e015efb304f7730
hafizhnafian <hafizhnafian@users.noreply.github.com>
2019-05-25 04:22:23 +0700

    Konversi Mata Uang

---
+++ moneyconvert/moneyconvert/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace moneyconvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Rupiah",
+            "Dollar",
+            "Euro",
+            "Pound"});
+            this.comboBox1.Location = new System.Drawing.Point(290, 159);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(112, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Rupiah",
+            "Dollar",
+            "Euro",
+            "Pound"});
+            this.comboBox2.Location = new System.Drawing.Point(511, 159);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(112, 24);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(511, 213);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "RESET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(424, 372);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "CONVERT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(261, 269);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(277, 57);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(290, 213);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::moneyconvert.Properties.Resources.TA_DKP;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(801, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ moneyconvert/moneyconvert/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace moneyconvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.ReadOnly = true;
+            textBox3.ReadOnly = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int awal;
+            int akhir;
+            double jumlah;
+            double hasil;
+
+            jumlah = Convert.ToDouble(textBox1.Text);
+            awal = comboBox1.SelectedIndex;
+            akhir = comboBox2.SelectedIndex;
+            if (comboBox1.SelectedIndex == 0)
+            {
+                if (jumlah >= 0)
+                {
+                    if (comboBox2.SelectedIndex == 0)
+                    {
+                        hasil = jumlah;
+                        textBox2.Text = "" + hasil;
+                    }
+                    else if (comboBox2.SelectedIndex == 1)
+                    {
+                        hasil = jumlah * 0.000069;
+                        textBox2.Text = "" + hasil;
+                    }
+                    else if (comboBox2.SelectedIndex == 2)
+                    {
+                        hasil = jumlah * 0.000062;
+                        textBox2.Text = "" + hasil;
+                    }
+                    else if (comboBox2.SelectedIndex == 3)
+                    {
+                        hasil = jumlah * 0.000055;
+                        textBox2.Text = "" + hasil;
+                    }
+                    textBox3.Text = "KONVERSI BERHASIL!!";
+                }
+                else
+                {
+                    textBox3.Text = "INPUTAN SALAH!!";
+                }
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                if (jumlah >= 0)
+                {
+                    if (comboBox2.SelectedIndex == 0)
+                    {
+                        hasil = jumlah * 14000;
+                        textBox2.Text = "" + hasil;
+                    }
+                    if (comboBox2.SelectedIndex == 1)
+                    {
+                        hasil = jumlah;
+                        textBox2.Text = "" + hasil;
+                    }
+                    if (comboBox2.SelectedIndex == 2)
+                    {
+                        hasil = jumlah * 0.90;
+                        textBox2.Text = "" + hasil;
+                    }
+                    if (comboBox2.SelectedIndex == 3)
+                    {
+                        hasil = jumlah * 0.79;
+                        textBox2.Text = "" + hasil;
+                    }
+                    textBox3.Text = "KONVERSI BERHASIL!!";
+                }
+                else
+                {
+                    textBox3.Text = "INPUTAN SALAH!!";
+                }
+            }
+            else if(comboBox1.SelectedIndex == 2)
+            {
+                if (jumlah >= 0)
+                {
+                    if (comboBox2.SelectedIndex == 0)
+                    {
+                        hasil = jumlah * 16000;
+                        textBox2.Text = "" + hasil;
+                    }
+                    if (comboBox2.SelectedIndex == 1)
+                    {
+                        hasil = jumlah * 1.12;
+                        textBox2.Text = "" + hasil;
+                    }
+                    if (comboBox2.SelectedIndex == 2)
+                    {
+                        hasil = jumlah;
+                        textBox2.Text = "" + hasil;
+                    }
+                    if (comboBox2.SelectedIndex == 3)
+                    {
+                        hasil = jumlah * 0.88;
+                        textBox2.Text = "" + hasil;
+                    }
+                    textBox3.Text = "KONVERSI BERHASIL!!";
+                }
+                else
+                {
+                    textBox3.Text = "INPUTAN SALAH!!";
+                }
+            }
+            else if(comboBox1.SelectedIndex == 3)
+            {
+                if (jumlah >= 0)
+                {
+                    if (comboBox2.SelectedIndex == 0)
+                    {
+                        hasil = jumlah * 18000;
+                        textBox2.Text = "" + hasil;
+                    }
+                    if (comboBox2.SelectedIndex == 1)
+                    {
+                        hasil = jumlah * 1.27;
+                        textBox2.Text = "" + hasil;
+                    }
+                    if (comboBox2.SelectedIndex == 2)
+                    {
+                        hasil = jumlah * 1.14;
+                        textBox2.Text = "" + hasil;
+                    }
+                    if (comboBox2.SelectedIndex == 3)
+                    {
+                        hasil = jumlah;
+                        textBox2.Text = "" + hasil;
+                    }
+                    textBox3.Text = "KONVERSI BERHASIL!!";
+                }
+                else
+                {
+                    textBox3.Text = "INPUTAN SALAH!!";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+            textBox2.Text = String.Empty;
+            textBox3.Text = String.Empty;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if(!char.IsDigit(ch) && ch != 8 && ch != 46 && ch != 45)
+            {
+                e.Handled = true; 
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
